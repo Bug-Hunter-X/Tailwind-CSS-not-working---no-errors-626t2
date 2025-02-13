50e627bbe159e5e9daccab37388cf5c1e13f5693
BugHunter-X <askzenai@gmail.com>
2025-02-13 11:46:11 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,9 @@
+```javascript
+//In your main app.js or equivalent file
+import 'tailwindcss/tailwind.css';
+
+//Then in your component file use a class like this
+<div className="bg-gray-200 p-4 rounded">
+  <p>Some text</p>
+</div>
+```
